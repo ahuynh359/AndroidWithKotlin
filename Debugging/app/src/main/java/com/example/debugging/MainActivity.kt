a6a2084a7190975a956da1d4ecfdd97864f96aa0
ahuynh0312 <ahuynh359@gmail.com>
2022-01-25 22:40:22 +0700

    Update

--- Debugging/app/src/main/java/com/example/debugging/MainActivity.kt
+++ Debugging/app/src/main/java/com/example/debugging/MainActivity.kt
@@ -2,12 +2,25 @@ package com.example.debugging
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        button.setOnClickListener{
+            Toast.makeText(this,"Ahihi",Toast.LENGTH_LONG).show()
+        }
         setContentView(R.layout.activity_main)
 
 
+
+
     }
+
+
+
 }
